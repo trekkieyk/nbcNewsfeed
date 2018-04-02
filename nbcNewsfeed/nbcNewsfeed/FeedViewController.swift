bc869c9a34ef6fe3b3b6e27ac0e7b19a8dcbd714
Yossi Katzin <joseph.katzin@gmail.com>
2018-04-02 10:32:18 -0700

    Initial code commit. Created project from the Single Page App template.

---
+++ nbcNewsfeed/nbcNewsfeed/FeedViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nbcNewsfeed
+//
+//  Created by Yossi Katzin on 4/2/18.
+//  Copyright © 2018 Yossi Katzin. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
